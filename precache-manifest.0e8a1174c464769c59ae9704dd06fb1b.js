ffea6c242daa4257a125cfab050de3cf74366be5
serateco <serateco@naver.com>
2019-09-04 20:25:08 +0900

    Updates

--- precache-manifest.0e8a1174c464769c59ae9704dd06fb1b.js
+++ precache-manifest.0e8a1174c464769c59ae9704dd06fb1b.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3270cfb19c81824b9d6b3e77067e3935",
+    "revision": "078fe620bc61dc9eaec479f84a86c979",
     "url": "/react-diary/index.html"
   },
   {
-    "revision": "cad5daebd25dfb08f39a",
+    "revision": "697407a2f6567d63ab68",
     "url": "/react-diary/static/css/main.a653e1c8.chunk.css"
   },
   {
-    "revision": "4b0fd634df4443a20eae",
-    "url": "/react-diary/static/js/2.4e14c3aa.chunk.js"
+    "revision": "1d3be312f3512064c58b",
+    "url": "/react-diary/static/js/2.a0063ac2.chunk.js"
   },
   {
-    "revision": "cad5daebd25dfb08f39a",
-    "url": "/react-diary/static/js/main.52bc9083.chunk.js"
+    "revision": "697407a2f6567d63ab68",
+    "url": "/react-diary/static/js/main.bb1d4474.chunk.js"
   },
   {
     "revision": "d2705297687b143d8b3d",
