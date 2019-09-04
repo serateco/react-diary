3e8b9985c2122fc37fb5440e349d93dcf44cc02a
serateco <serateco@naver.com>
2019-09-04 15:44:27 +0900

    Updates

--- precache-manifest.1e14cf95d05e34b71522558025d72011.js
+++ precache-manifest.1e14cf95d05e34b71522558025d72011.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "95eb1a4dd8a8e743bc1d9d1ebb0bd5f7",
+    "revision": "98d74ca5010b147a982658de968282b3",
     "url": "/react-diary/index.html"
   },
   {
-    "revision": "f588c442c44de77642f8",
-    "url": "/react-diary/static/css/main.0c90369b.chunk.css"
+    "revision": "a33822257a503fa466dd",
+    "url": "/react-diary/static/css/main.a653e1c8.chunk.css"
   },
   {
     "revision": "98be52014c8ee606a693",
     "url": "/react-diary/static/js/2.e5337c58.chunk.js"
   },
   {
-    "revision": "f588c442c44de77642f8",
+    "revision": "a33822257a503fa466dd",
     "url": "/react-diary/static/js/main.3a407a77.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-diary/precache-manifest.8dd9ff5ed8776e72b21598aa26aeb510.js"
+  "/react-diary/precache-manifest.1e14cf95d05e34b71522558025d72011.js"
 );
 
 self.addEventListener('message', (event) => {
